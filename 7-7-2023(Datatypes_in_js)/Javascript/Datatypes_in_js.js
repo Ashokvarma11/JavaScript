2d66416d23de0230ace08580eea202ee45db7597
Ashokvarma11 <surampudiashokvarma111@gmail.com>
2023-07-16 14:31:55 +0530

    DataTypes_in_js

---
+++ 7-7-2023(Datatypes_in_js)/Javascript/Datatypes_in_js.js
@@ -0,0 +1,117 @@
+// Datatypes in js 
+console.log("///Example1///");
+console.log("---Number datatype----");
+let a;
+a=10;
+console.log(a);
+// a is a identifier and 20 is value which is datatype of Number
+console.log("---String datatype----");
+var myName="Ashok Varma";
+console.log(myName);
+// here  myName is a variable name which contains string data i.e Ashok Varma
+console.log("---Boolean datatype----");
+// Boolean datatype contains two values either true or false.
+const bool=true;
+console.log(bool);
+console.log("---Undefined datatype----");
+ let d;
+ console.log(d);
+ //here d is not assigned with value so javascript engine assigns it as undefined
+ console.log("--- Null datatype----");
+ let e=null;
+ console.log(e)
+//  here if you dont want js engine to assign it as undefined and u want to assign it as null u will use null
+console.log("---Array datatype----");
+let personalData=['ashok','varma','surampudi'];
+console.log(personalData);
+// Array is used to store the data
+console.log("---Object datatype----");
+const studentData={
+    name:"Ashok",
+    age:23,
+    status:"single"
+}
+console.log(studentData);
+// here studentData is a Object which stores group of data
+
+
+console.log("///Example2///");
+console.log("---Number datatype----");
+let myFavNum=1;
+console.log(myFavNum);
+console.log("---String datatype----");
+var myCourse="Btech";
+console.log(myCourse);
+console.log("---Boolean datatype----");
+const bool1=false;
+console.log(bool1);
+console.log("---Array datatype----");
+let numberData=[1,2,3,4,5];
+console.log(numberData);
+console.log("---Object datatype----");
+const myData={
+    name:"varma",
+    age:23,
+    address:"AP"
+}
+console.log(myData);
+
+
+console.log("///Example3///");
+console.log("---Number datatype----");
+let myRoleNum=1;
+console.log(myRoleNum);
+console.log("---String datatype----");
+var myFavRole="Developer";
+console.log(myFavRole);
+console.log("---Array datatype----");
+let combinedData=[1,"ashok",3,4,"student"];
+console.log(combinedData);
+console.log("---Object datatype----");
+const instituteData={
+    institute:"10k",
+    fee:"40k",
+      duration:"3 months"
+}
+console.log(instituteData);
+
+
+console.log("///Example4///");
+console.log("---Number datatype----");
+let myId=160819;
+console.log(myId);
+console.log("---String datatype----");
+var learningCourse="React";
+console.log(learningCourse);
+console.log("---Array datatype----");
+let javaScriptFrameworks=["react","angular","node"];
+console.log(javaScriptFrameworks);
+console.log("---Object datatype----");
+const offers={
+    offerstillnow:"infosys",
+    update:"rejected",
+}
+console.log(offers);
+
+
+console.log("///Example5///");
+console.log("---Number datatype----");
+const v=18;
+console.log(v);
+console.log("---String datatype----");
+var favCricketer="Virat";
+console.log(favCricketer);
+console.log("---Array datatype----");
+let js=[{usage:"frontend",founded:"1995",
+languageType:"scripting"
+    
+}, {
+    demand:"popular",scope:"lot of scope"
+}];
+//array of objects 
+console.log(js);
+console.log("---Object datatype----");
+const objInDifLang={
+ hashtable:" In c",dictinaries:"in python", hashmap:"java"   
+}
+console.log(objInDifLang);
